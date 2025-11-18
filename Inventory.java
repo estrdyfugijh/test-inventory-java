3b98d4766116ea39c82073750dabe06ed1299d6b
estrdyfugijh <127370741+estrdyfugijh@users.noreply.github.com>
2025-11-18 11:08:28 -0600

    Create Inventory.java

---
+++ Inventory.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Inventory {
+
+
+  public void close() {
+    
+  }
+}
